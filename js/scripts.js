0b20b2bac1f20442713c0a65dd90631bfa7c6e7d
amyvs <amydecat@yahoo.com>
2014-07-12 13:51:33 -0700

    replaced ';' in dictionaries with ','

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 var scrabbleScore = function(word) {
 	//Dictionaries
-	var onePt = {"a": 1; "e": 1; "i": 1; "o": 1; "u": 1; "l": 1; "n": 1; "r": 1; "s": 1; "t": 1};
-	var twoPt = {"d": 2; "g": 2};
-	var threePt = {"b": 3; "c": 3; "m": 3; "p": 3};
-	var fourPt = {"f": 4; "h": 4; "v": 4; "w": 4; "y": 4};
+	var onePt = {"a": 1, "e": 1, "i": 1, "o": 1, "u": 1, "l": 1, "n": 1, "r": 1, "s": 1, "t": 1};
+	var twoPt = {"d": 2, "g": 2};
+	var threePt = {"b": 3, "c": 3, "m": 3, "p": 3};
+	var fourPt = {"f": 4, "h": 4, "v": 4, "w": 4, "y": 4};
 	var fivePt = {"k": 5};
-	var eightPt = {"j": 8; "x": 8}
-	var tenPt = {"q": 10; "z": 10}
+	var eightPt = {"j": 8, "x": 8};
+	var tenPt = {"q": 10, "z": 10};
 
 	//User input manipulation
 	var wordSplit = word.toLowerCase().split("");
+
+	var ptArr = [];
+
+	for (var i = 0; i < word.length; i++) {
+		if (wordSplit[i] === onePt[i]) {
+			ptArr.push(onePt.valueOf[i]);
+			console.log(ptArr);
+			return ptArr;
+		}
+	}
 	
 }
