f14f62ca0014e16fd2f4d377bba32035dde818e6
amyvs <amydecat@yahoo.com>
2014-07-12 13:38:25 -0700

    added dictionaries to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-var scrabbleScore = function() {
-	return false;
+var scrabbleScore = function(word) {
+	//Dictionaries
+	var onePt = {"a": 1; "e": 1; "i": 1; "o": 1; "u": 1; "l": 1; "n": 1; "r": 1; "s": 1; "t": 1};
+	var twoPt = {"d": 2; "g": 2};
+	var threePt = {"b": 3; "c": 3; "m": 3; "p": 3};
+	var fourPt = {"f": 4; "h": 4; "v": 4; "w": 4; "y": 4};
+	var fivePt = {"k": 5};
+	var eightPt = {"j": 8; "x": 8}
+	var tenPt = {"q": 10; "z": 10}
+
+	//User input manipulation
+	var wordSplit = word.toLowerCase().split("");
+	
 }
